39824f995984672c44f44354fa547ccf9375e490
Masha <rezzo.qm@gmail.com>
2024-12-07 12:11:58 +0300

    edit for github pages

--- src/main/java/mashas/pro/controllers/web/MainPageController.java
+++ src/main/java/mashas/pro/controllers/web/MainPageController.java
@@ -14,10 +14,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainPageController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    @GetMapping("/index")
+    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "index";
     }
 
     @GetMapping("/maps")
